269c83df561c001253bdbd57b2a66bfbf73fdd1e
ichiyajou0125 <120617535+ichiyajou0125@users.noreply.github.com>
2025-02-23 22:30:53 +0900

    フレーム、フレームの移動・それに合わせたマスの可視化の実装

--- Character_Manager.cs
+++ Character_Manager.cs
@@ -12,50 +12,92 @@ public class Character_Manager : MonoBehaviour
         x = (int)floatx + 9;
         z = (int)floatz + 9;
         stage[x][z] = true;
-        Debug.Log("CM: x = " + x + ", z = " + z);
+        //Debug.Log("CM: x = " + x + ", z = " + z);
     }
-        public void Jage_nextPostion(List<List<bool>> stage,int x,int z, int[,] arr_NextPos, ref int NextPos_num){
+    public void Jage_nextPostion(List<List<bool>> stage,
+                                 List<List<int>> WolkRange_postion,
+                                 int x,
+                                 int z,
+                                 List<List<int>>  arr_NextPos,
+                                 ref int NextPos_num){
         int[] jage_case = {-1,1};
+        List<int> row;
         for(int i = 0; i < 2; i++){
-            if(IsValidIndex(stage,x + jage_case[i],z) && stage[x + jage_case[i]][z] == false){ //左・右のIndexが有効で、かつ探索されていない
-                stage[x + jage_case[i]][z] = true;
-                arr_NextPos[NextPos_num,0] = x + jage_case[i];
-                arr_NextPos[NextPos_num,1] = z;
+            List<int> nextLook_postion = new List<int> { x + jage_case[i], z };
+            if(IsValidIndex(stage,x + jage_case[i],z) && list_jage(WolkRange_postion,nextLook_postion)){ //左・右のIndexが有効で、かつ探索されていない
+                row = new List<int>();
+                row.Add(x + jage_case[i] - 9);
+                row.Add(z - 9);
+                WolkRange_postion.Add(row);
+                arr_NextPos.Add(row);
+
                 NextPos_num++;
             }
-            if(IsValidIndex(stage,x,z + jage_case[i]) && stage[x][z + jage_case[i]] == false){ //上・下のIndexが有効で、かつ探索されていない
-                stage[x][z + jage_case[i]] = true;
-                arr_NextPos[NextPos_num,0] = x;
-                arr_NextPos[NextPos_num,1] = z + jage_case[i];
+            nextLook_postion = new List<int>{x, z + jage_case[i]};
+            if(IsValidIndex(stage,x,z + jage_case[i]) && list_jage(WolkRange_postion,nextLook_postion)){ //上・下のIndexが有効で、かつ探索されていない
+                row = new List<int>();
+                row.Add(x - 9);
+                row.Add(z + jage_case[i] - 9);
+                WolkRange_postion.Add(row);
+                arr_NextPos.Add(row);
+
                 NextPos_num++;
             }
         }
     }
-    public void walk_range(List<List<bool>> stage,int x, int z,int stage_size,GameObject WolkSize_Prefab){
+    public void walk_range(List<List<bool>> stage,int x, int z,ref int Wolk_width,ref List<List<int>> WolkRange_postion){
         int fornum;
         int LookPos_num = 1;//今見ている場所が何個あるのか
-        int[,] LookPos_arr = new int[1,2];
-        LookPos_arr[0,0] = x;
-        LookPos_arr[0,1] = z;
-        for(int i = 0; i < 3; i++){//配列に次の移動できる場所を格納。それを参照して次の移動マスを判定する
-            int[,] NextPos_arr = new int[LookPos_num + 4,2];
+
+        List<List<int>> LookPos_arr = new List<List<int>>();
+        List<int> row = new List<int>{x-9,z-9};
+        LookPos_arr.Add(row);
+
+        WolkRange_postion = new List<List<int>>();
+
+        for(int i = 0; i < Wolk_width; i++){//配列に次の移動できる場所を格納。それを参照して次の移動マスを判定する
+            List<List<int>> NextPos_arr = new List<List<int>>();
             fornum = LookPos_num;
             LookPos_num = 0;
             for(int j = 0; j < fornum; j++){
-                Jage_nextPostion(stage,LookPos_arr[j,0],LookPos_arr[j,1],NextPos_arr,ref LookPos_num);
+                Jage_nextPostion(stage,WolkRange_postion,LookPos_arr[j][0] + 9,LookPos_arr[j][1] + 9,NextPos_arr,ref LookPos_num);
             }
-            LookPos_arr = new int[LookPos_num+4,2];
             LookPos_arr = NextPos_arr;
         }
-
-        for(int i = 0; i < stage_size; i++){
-            for(int j = 0; j < stage_size; j++){
-                if(stage[i][j]){
-                    Instantiate(WolkSize_Prefab,new Vector3(i-9,-0.04f,j-9),Quaternion.identity);
-                }
-            }
+    }
+    public void LookPlace(List<List<bool>> stage, ref int x, ref int z, GameObject LookPrace_frame){
+        if(Input.GetKeyDown(KeyCode.UpArrow) && IsValidIndex(stage,x - 1,z)){
+            x--;
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow) && IsValidIndex(stage,x + 1,z)){
+            x++;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && IsValidIndex(stage,x,z + 1)){
+            z++;
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && IsValidIndex(stage,x,z - 1)){
+            z--;
+        }
+        LookPrace_frame.transform.position = new Vector3(x-9,0,z-9);
+    }
+    public void visualize_WolkRange(List<List<int>> WolkRange_postion,int stage_size,GameObject WolkRange_Prefab,GameObject Wolk_Range){
+        for(int i = 0; i < WolkRange_postion.Count; i++){
+            GameObject Prefab = Instantiate(WolkRange_Prefab,Wolk_Range.transform);
+            Prefab.transform.position = new Vector3(WolkRange_postion[i][0],-0.01f,WolkRange_postion[i][1]);
         }
     }
+    public bool list_jage(List<List<int>> postion_list, List<int> next_postion)
+    {
+        foreach (List<int> child in postion_list)
+        {
+            if (child[0] == next_postion[0] && child[1] == next_postion[1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     bool IsValidIndex(List<List<bool>> grid, int row, int col)
     {
         return row >= 0 && row < grid.Count &&         // 行数チェック
--- Game_Manager.cs
+++ Game_Manager.cs
@@ -7,11 +7,13 @@ public class Game_Manager : MonoBehaviour
     public Character_Manager CM;
     public GameData gameData;
     GameObject Character;
+    public GameObject LookPrace_frame;
     public GameObject WolkRange_Prefab;
-
+    public GameObject Wolk_Range;
     int stage_size = 20;
     int x,z;
-
+    int Wolk_width = 3;
+    List<List<int>> WolkRange_postion;
     void Start()
     {
         Character = GameObject.Find("Capsule");
@@ -23,15 +25,24 @@ public class Game_Manager : MonoBehaviour
             }
             gameData.stagedata.Add(row);
         }
+        CM.get_postion(Character,gameData.stagedata,ref x,ref z);
+        CM.get_postion(Character,gameData.stagedata,ref x, ref z);
+        LookPrace_frame.transform.position = new Vector3(x-9,0,z-9);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            CM.get_postion(Character,gameData.stagedata,ref x,ref z);
-            CM.walk_range(gameData.stagedata,x,z,stage_size,WolkRange_Prefab);
-            CM.PrintList(gameData.stagedata);
+        if(gameData.stagedata[x][z]){
+            CM.walk_range(gameData.stagedata,x,z,ref Wolk_width,ref WolkRange_postion);
+            CM.visualize_WolkRange(WolkRange_postion,stage_size,WolkRange_Prefab,Wolk_Range);
         }
+        else{
+            foreach(Transform child in Wolk_Range.transform){
+                Destroy(child.gameObject);
+            }
+        }
+        CM.LookPlace(gameData.stagedata,ref x,ref z,LookPrace_frame);
+        CM.PrintList(gameData.stagedata);
     }
 }
